941ebec6da5ea2d3ea1dad23ae4e8947650ec8f0
KetUnderscore <tannersock14@gmail.com>
2024-07-10 19:16:30 -0700

    Port deploying grah

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.DB_URI, dbOptions)
 .then(() => console.log('DB Connected!'))
 .catch((err) => console.log(err))
 
-const port = 4000
+const port = process.env.PORT || 4000
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
